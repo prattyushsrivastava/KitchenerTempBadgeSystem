84f5a7f0a5ce76631eea18a3d12cc07cd3f3c5c7
priya-tarkar <priya.gla_cs19@gla.ac.in>
2022-12-22 13:56:14 +0530

    first commit

--- Presentation.Layer/Controllers/HomeController.cs
+++ Presentation.Layer/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Presentation.Layer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
     }
 }
